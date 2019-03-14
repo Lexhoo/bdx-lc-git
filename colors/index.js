aef2973176e72eb06e77c0d95c887b816318b33a
AlexLoWCD <alexandre.lorion@wildcodeschool.fr>
2019-03-14 16:54:32 +0100

    ajout colors

---
+++ colors/index.js
@@ -0,0 +1,15 @@
+// On récupère tous les boutons "apply"
+const btnsApply = document.getElementsByClassName('apply-color');
+
+// On parcours tous les boutons pour leur appliquer l'écoute à l'évènement du clic
+for (let i = 0; i < btnsApply.length; i++) {
+  const btn = btnsApply[i];
+
+  // Ajout de l'écoute
+  btn.addEventListener('click', function() {
+    // Récupération de la couleur, 
+    const sampleColor = this.parentElement.previousElementSibling.style.backgroundColor;
+    // Application de la couleur au "body"
+    document.body.style.backgroundColor = sampleColor;
+  });
+}
---
+++ colors/test.js
@@ -0,0 +1,9 @@
+const t = [12, 46, 5798, 57839, 279321];
+
+let sum2 = 0;
+for (let n = 0; n < t.length; n += 1) {
+  const num = t[n];
+  sum2 = sum2 + num;
+}
+console.log(sum2);
+
